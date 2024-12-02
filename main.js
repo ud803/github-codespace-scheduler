bf9203c8a953c62fdff625e939b1a6c892adc355
Lee Ui Dam <ud803da@gmail.com>
2024-12-02 23:38:28 +0900

    d

--- main.js
+++ main.js
@@ -24,28 +24,27 @@ try {
       })
 
     // GitHub API 요청 (octokit 사용)
-    const response = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/stop`, {
+    const response_start = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/start`, {
         codespace_name: CODESPACE_NAME,
         headers: {
           'X-GitHub-Api-Version': '2022-11-28'
         }
       });
+      // API 응답 출력 (선택적)
+      console.log(response_start);
+      
+      await new Promise(r => setTimeout(r, 20));
+      console.log('Sleep 20 secs');
+      
+      const response_stop = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/start`, {
+        codespace_name: CODESPACE_NAME,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      });
+      // API 응답 출력 (선택적)
+      console.log(response_stop);
 
-    // const response = await octokit.request('POST /user/codespaces', {
-    //     repository_id: repo_id,
-    //     ref: 'main',
-    //     geo: 'UsWest',
-    //     headers: {
-    //         'X-GitHub-Api-Version': '2022-11-28'
-    //     },
-    //     pull_request: {
-    //         pull_request_number: PR_NUMBER
-    //         ,repository_id: repo_id
-    //     }
-    // });
-
-    // API 응답 출력 (선택적)
-    console.log(response);
 
 } catch (error) {
     // 에러 발생 시 처리
