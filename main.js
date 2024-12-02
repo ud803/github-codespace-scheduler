82210dce21e7dcdf6d0beb33dded537705f794a3
Lee Ui Dam <ud803da@gmail.com>
2024-12-03 00:37:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,33 +11,35 @@ try {
     const CODESPACE_NAME = core.getInput('CODESPACE_NAME');
     const WAIT_SECONDS = core.getInput('WAIT_SECONDS');
 
-    console.log(`Hello my repo_id is: , ${REPO_ID}!`);
+    console.log(`Hello my repo_id is: ${REPO_ID}!`);
 
     const octokit = new Octokit({
         auth: GITHUB_AUTH
       })
 
     // 1. Start Codespace
+    console.log(`API:: POST /user/codespaces/${CODESPACE_NAME}/start`)
     const response_start = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/start`, {
-        codespace_name: CODESPACE_NAME,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      });
-      console.log(response_start);
-      
-      // 2. Wait sometimes until some job is done under codespace.
-      console.log(`Wait for ${WAIT_SECONDS} seconds...`);
-      await new Promise(r => setTimeout(r, WAIT_SECONDS * 1000));
-      
-      // 3. Stop Codespace
-      const response_stop = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/stop`, {
-        codespace_name: CODESPACE_NAME,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      });
-      console.log(response_stop);
+      codespace_name: CODESPACE_NAME,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(response_start);
+    
+    // 2. Wait sometimes until some job is done under codespace.
+    console.log(`Wait for ${WAIT_SECONDS} seconds...`);
+    await new Promise(r => setTimeout(r, WAIT_SECONDS * 1000));
+    
+    // 3. Stop Codespace
+    console.log(`API:: POST /user/codespaces/${CODESPACE_NAME}/stop`)
+    const response_stop = await octokit.request(`POST /user/codespaces/${CODESPACE_NAME}/stop`, {
+      codespace_name: CODESPACE_NAME,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log(response_stop);
 
 } catch (error) {
     // Error Message
