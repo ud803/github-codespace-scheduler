fe841f48abc02f5827d009a42407d0558378885b
Lee Ui Dam <ud803da@gmail.com>
2024-12-02 22:07:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const core = require('@actions/core');
 async function run() {
     try {
         const name = core.getInput('name');
+        const name2 = core.getInput('name2');
         console.log(`Hello, ${name}!`);
+        console.log(`Hello, ${name2}!`);
     } catch (error) {
         core.setFailed(`Action failed with error: ${error.message}`);
     }
