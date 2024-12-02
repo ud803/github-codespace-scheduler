54db5b8209adb3cb080fcee399314b9e327e4d10
Lee Ui Dam <ud803da@gmail.com>
2024-12-02 22:41:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const { Octokit } = require('@octokit/core'); // Octokit 가져오기
 
 // Octokit.js
 // https://github.com/octokit/core.js#readme
-const github_auth = core.getInput('auth');
 
-const octokit = new Octokit({
-    auth: github_auth
-  })
+
 
 async function run() {
 try {
     // 입력 값 가져오기
     const name = core.getInput('name');
     const name2 = core.getInput('name2');
-    
+    const github_auth = core.getInput('github_auth');
+
     // 로그 출력
     console.log(`Hello, ${name}!`);
     console.log(`Hello, ${name2}!`);
+
+    const octokit = new Octokit({
+        auth: github_auth
+      })
 
     // GitHub API 요청 (octokit 사용)
     const response = await octokit.request('POST /user/codespaces', {
