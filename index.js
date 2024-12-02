df572782ad32af2615af738fa0a5a4015b94869f
Lee Ui Dam <ud803da@gmail.com>
2024-12-02 22:36:30 +0900

    dd

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const core = require('@actions/core');
 
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: 'YOUR-TOKEN'
+  })
+  
+
 async function run() {
     try {
         const name = core.getInput('name');
@@ -12,3 +19,13 @@ async function run() {
 }
 
 run();
+
+await octokit.request('POST /user/codespaces', {
+    repository_id: "R_kgDONXvtSA",
+    ref: 'main',
+    geo: 'UsWest',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+  
