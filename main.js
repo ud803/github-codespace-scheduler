5d779173f6b69bddea9e3f1c3132669176bbcfb1
Lee Ui Dam <ud803da@gmail.com>
2024-12-03 19:32:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ try {
 
     // Fetch Variables
     const CODESPACE_NAME = core.getInput('CODESPACE_NAME');
-    const WAIT_SECONDS = core.getInput('WAIT_SECONDS');
 
     const octokit = new Octokit({
         auth: GITHUB_AUTH
@@ -24,20 +23,6 @@ try {
     });
     console.log(response_start);
     
-    // 2. Wait sometimes until some job is done under codespace.
-    console.log(`Wait for ${WAIT_SECONDS} seconds...`);
-    await new Promise(r => setTimeout(r, WAIT_SECONDS * 1000));
-    
-    // 3. Stop Codespace
-    console.log(`API:: POST /user/codespaces/{codespace_name}/stop`)
-    const response_stop = await octokit.request('POST /user/codespaces/{codespace_name}/stop', {
-      codespace_name: CODESPACE_NAME,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    });
-    console.log(response_stop);
-
 } catch (error) {
     // Error Message
     core.setFailed(`Action failed with error: ${error.message}`);
